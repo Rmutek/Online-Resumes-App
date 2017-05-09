9dfae8b1996211577fb41885082db67af9ad421c
Linda Stettler <lstettle@yahoo.com>
2017-05-08 19:18:47 -0500

    Added comment LS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #More notes. Enjoy. 
   #test this process - KG
+  #yet more notes!! - LS
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
